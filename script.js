679cdc984f8fbcfa7c2de2cff2868fc709e35392
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-01-07 20:15:52 -0700

    done

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
 let AddTask = () => {
   if (inputBox.value === "") {
     alert("You must write something");
@@ -14,13 +18,26 @@ let AddTask = () => {
     li.appendChild(span);
 
     inputBox.value = "";
+    saveData();
   }
 };
 
-listContainer.addEventListener("click", function (e) {
-  if (e.target.tagName === "LI") {
-    e.target.classList.toggle("checked");
-  } else if (e.target.tagName === "SPAN") {
-    e.target.parentElement.remove();
-  }
-});
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function showList() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showList();
